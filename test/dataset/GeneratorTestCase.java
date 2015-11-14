1336aa0f875e4d41cfa58039b554286cff61d6ba
Christian Koehler <sandvice@gmail.com>
2015-11-14 17:44:41 -0500

    starting tests

---
+++ test/dataset/GeneratorTestCase.java
@@ -0,0 +1,18 @@
+package dataset;
+
+import org.junit.Test;
+
+/**
+ * Created by Christian on 11/14/2015.
+ */
+public class GeneratorTestCase {
+    MasterSet  masterSet;
+
+    @Test
+    public void testRoutine(){
+        masterSet = new MasterSet();
+        masterSet.randomize(2,3,4);
+        //System.out.println(masterSet.toString());
+    }
+
+}
