6dd516805027881ea5da8977f218447726c0ec37
noahpoz <noahpocz@buffalo.edu>
2015-11-24 18:23:47 -0500

    Created UI.

--- src/ui/UI.java
+++ src/ui/UI.java
@@ -1,132 +1,73 @@
 package ui;
 
-
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.AlphaComposite;
+import java.awt.Dimension;
+import java.awt.Point;
 import java.util.ArrayList;
 
 import javax.swing.*;
 
-import java.util.List;
 
 public class UI implements Runnable {
-
-	JFrame _mainWindow;
-	JPanel _contentPane;
 	
-	public static int WINDOW_WIDTH = 900;
-	public static int WINDOW_HEIGHT = 450;
+	public static int WINDOW_WIDTH = 1050;
+	public static int WINDOW_HEIGHT = 525;
+	private int _paneHeight;
+	public static int PADDING = 12;
+	
+	private JFrame _window;
+	private JLayeredPane _mainContentPane;
+	
+//	private Model _model;
 	
 	public UI() {
 		
+		
+		
+	}
+
+	public static void formatJComponent(JComponent j, Dimension d, int x, int y) {
+		j.setSize(d);
+		j.setLocation(x, y);
 	}
 	
+	public static AlphaComposite generateAlpha(float alpha) {
+		//http://www.javaworld.com/article/2076733/java-se/antialiasing--images--and-alpha-compositing-in-java-2d.html?page=2
+		AlphaComposite a = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha);
+		return a;
+	}
+
 	@Override
 	public void run() {
 		
-		_mainWindow = new JFrame("CSE115 Object Diagram Practice");
-		_mainWindow.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		//setting up main frame and main panel
+		_window = new JFrame("Object Diagram Analysis");
+		_mainContentPane = new JLayeredPane();
+		_mainContentPane.setLayout(null);
+		_window.setContentPane(_mainContentPane);
+		_window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		
-		_contentPane = new JPanel();
-		_contentPane.setLayout(null);
-		_mainWindow.setContentPane(_contentPane);
+		_window.setVisible(true);
+		_paneHeight = _window.getRootPane().getHeight();
 		
-		// ***** SETTING UP CODE AREA *****
-		CodeArea code = new CodeArea(this);
-		formatJComponent(code, new Dimension(WINDOW_WIDTH / 2, WINDOW_HEIGHT), 0, 0);
-		_contentPane.add(code);
+		// *** SETTING UP CODE VIEWER
+		CodeArea codeView = new CodeArea(this);
+		UI.formatJComponent(codeView, new Dimension(WINDOW_WIDTH / 2, WINDOW_HEIGHT), 0, 0);
+		_mainContentPane.add(codeView);
 		
-		// ***** SETTING UP EDITOR PANE *****
-		DrawingPanel jPanel = new DrawingPanel();
-        JPanel buttonHolder = new JPanel();
-
-        formatJComponent(jPanel, new Dimension(WINDOW_WIDTH /2, WINDOW_HEIGHT), WINDOW_WIDTH /2 , 0);
-        formatJComponent(buttonHolder, new Dimension(200, 200), WINDOW_WIDTH / 2 , 300);
-
-        JLabel jLabel = new JLabel("Edit Object Diagram Here:");
-        
-        JButton arrow = new JButton("ARROW");
+		// *** SETTING UP THE DIAGRAM EDITOR
+		EditorPane edit = new EditorPane(this);
+		UI.formatJComponent(edit, new Dimension(WINDOW_WIDTH / 2 - PADDING * 2, _paneHeight - PADDING * 2), 
+				WINDOW_WIDTH / 2 + PADDING, PADDING);
+		_mainContentPane.add(edit);
 		
-        JButton circle = new JButton("CIRCLE");
-		circle.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Circle c = new Circle(100, 100, 50, 50);
-		        jPanel.addCircle(c);
-			}
-		});
-		
-		JButton square = new JButton("SQUARE");
-
-        arrow.setBounds(WINDOW_WIDTH /2, 40, 100, 50);
-        circle.setBounds(WINDOW_WIDTH /2, 90 ,100 ,50);
-		square.setBounds(WINDOW_WIDTH / 2, 140, 100, 50);
-
-        
-        
-        
-
-		_contentPane.add(arrow);
-		_contentPane.add(circle);
-		_contentPane.add(square);
-        jPanel.add(jLabel);
-        jPanel.add(buttonHolder);
-        buttonHolder.setVisible(true);
-        jPanel.setVisible(true);
-		_contentPane.add(jPanel);
-		_mainWindow.setVisible(true);
-		
-		
+	}	
+	
+	public int getPaneHeight() {
+		return _paneHeight;
 	}
 	
-	//to cut down on code repetition
-	public static void formatJComponent(JComponent c, Dimension size, int x, int y) {
-		c.setSize(size);
-		c.setLocation(x, y);
+	public Point getLocation() {
+		return _window.getLocation();
 	}
-	
-	public int windowPaneHeight() {
-		return _mainWindow.getLayeredPane().getHeight();
-	}
-
-    class Circle {
-        int x,y, width, height;
-
-        public Circle(int x, int y, int width, int height){
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-        }
-
-        public void draw(Graphics g){
-            g.drawOval(x, y, width, height);
-        }
-
-    }
-    class DrawingPanel extends JPanel {
-        List <Circle> circles = new ArrayList<>();
-
-        @Override
-        protected void paintComponent(Graphics g){
-            super.paintComponent(g);
-            for(Circle circle : circles){
-                circle.draw(g);
-            }
-        }
-
-        public void addCircle(Circle circle){
-            circles.add(circle);
-            repaint();
-        }
-
-        @Override
-        public Dimension getPreferredSize(){
-            return new Dimension(100,100);
-        }
-
-    }
 }
---
+++ src/ui/shapes/Shape.java
@@ -0,0 +1,117 @@
+package ui.shapes;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.geom.Ellipse2D;
+
+import ui.UI;
+
+public abstract class Shape {
+
+	protected boolean _defunct;
+
+	protected double _x;
+	protected double _y;
+
+	protected int _alphaOscillation;
+	protected int _alphaDirection;
+
+	protected String _name;
+
+	public Shape(double x, double y) {
+		_x = x;
+		_y = y;
+
+		_alphaOscillation = 0;
+		_alphaDirection = 2;
+
+		_name = "untitled";
+	}
+
+	public void draw(Graphics2D g2, int state, boolean preliminary) {
+
+		if (state == 0) {
+			g2.setColor(Color.BLACK); //inactive
+		} else if (state == 1) {
+			g2.setColor(Color.GREEN);   //active
+		} else if (state ==2) {
+			g2.setColor(new Color(148,0,211));  //targeted [purple]
+		} else if (state ==3) {
+			g2.setColor(Color.RED); //superimposed
+		}
+
+		if (preliminary) {
+			//set translucency level
+			//http://www.javaworld.com/article/2076733/java-se/antialiasing--images--and-alpha-compositing-in-java-2d.html?page=2
+			g2.setComposite(UI.generateAlpha(_alphaOscillation / 100.0f));
+			_alphaOscillation += _alphaDirection;
+			if (_alphaOscillation > 98) {
+				_alphaDirection = -2;
+			} else if (_alphaOscillation < 2) {
+				_alphaDirection = 2;
+			}
+		}
+
+		BasicStroke solid = new BasicStroke(2.5f);
+		g2.setStroke(solid);
+	}
+
+	public boolean isTouching(Shape s) {
+
+		if (!this.equals(s)) {
+
+			//distance between centers of any two shapes can be used universally
+			double deltaX = (this.getX() - s.getX());
+			double deltaY = (this.getY() - s.getY());
+			double distance = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
+
+			if (this.getType().equals("Object") && s.getType().equals("Object")) {
+				if (distance <= ObjectShape.RADIUS * 2) {
+					return true;
+				}
+			}
+			
+			if (!this.getType().equals(s.getType())) {
+				if (distance <= (ObjectShape.RADIUS + VariableShape.SIDE / 2)) {
+					return true;
+				}
+			}
+
+			if (this.getType().equals("Variable") && s.getType().equals("Variable")) {
+				boolean xInRange = (Math.abs((double) (this.getX() - s.getX())) < VariableShape.SIDE);
+				boolean yInRange = (Math.abs((double) (this.getY() - s.getY())) < VariableShape.SIDE);
+				if (xInRange && yInRange) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public void setName(String name) {
+		_name = name;
+	}
+
+	public abstract double distanceFromCenter(double x, double y);
+	public abstract boolean contains(double x, double y);
+
+	public abstract double getX();
+	public abstract double getY();
+
+	public abstract void setX(double x);
+	public abstract void setY(double y);
+
+	public abstract String getType();
+
+	public abstract void dispose();
+
+	public boolean isDefunct() {
+		return _defunct;
+	}
+}
